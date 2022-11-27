56f388a17bee4d41a01a7dd8ba902100e1dd6056
Jonas Braga <jonasbraga2001@gmail.com>
2022-11-27 15:35:19 -0300

    refactor: :wastebasket: cleaning up old unnecessary files

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -1,24 +1,5 @@
-const {
-  HISTORY_TABLE_NAME,
-  CONTEXT_TABLE_NAME_PREFIX,
-  WEB_CRAWLER_STATE_MACHINE_ARN,
-} = process.env;
-
-/**
- * Responsible for starting a crawl
- */
 export const getTrips = async (event: any, context: any) => {
   console.log(event, context);
-
-  if (
-    !HISTORY_TABLE_NAME ||
-    !WEB_CRAWLER_STATE_MACHINE_ARN ||
-    !CONTEXT_TABLE_NAME_PREFIX
-  ) {
-    throw new Error(
-      "Environment not configured correctly. HISTORY_TABLE_NAME, WEB_CRAWLER_STATE_MACHINE_ARN and CONTEXT_TABLE_NAME_PREFIX must be specified"
-    );
-  }
 
   // TODO....
 };
