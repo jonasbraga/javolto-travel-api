1ce2c7bf56fe410ba7e004e28f0e989dd54d0329
anderlr13 <anderlr13@gmail.com>
2022-11-29 20:16:13 -0300

    incluindo apis

---
+++ src/lambda/crawlers/constants/index.ts
@@ -0,0 +1,8 @@
+export const cityID = { 
+  Berlin: 3000035821,
+  Moscow: 5000406639,
+  Paris: 3000035827,
+  Dublin:3000035826,
+  Amsterdam: 3000035824,
+  London: 3000035825
+} as const;
--- src/lambda/crawlers/hotel-crawler.ts
+++ src/lambda/crawlers/hotel-crawler.ts
@@ -1,30 +1,29 @@
-import * as AWS from "aws-sdk";
-import { updateHistoryEntry } from "../utils/cacheTable";
-import { CrawlContext } from "../crawler/types";
-
-const sfn = new AWS.StepFunctions();
+//import * as AWS from "aws-sdk";
+//import { updateHistoryEntry } from "../utils/cacheTable";
+//import { CrawlContext } from "../crawler/types";
+import axios from 'axios';
+//const sfn = new AWS.StepFunctions();
 
 /**
  * Continue execution of the crawler in another step function execution
  */
-export const hotelCrawl = async (crawlContext: CrawlContext) => {
-  console.log("Continuing execution for", crawlContext);
-
-  console.log("Resetting batch count for next state machine execution");
-  await updateHistoryEntry(crawlContext.crawlId, {
-    batchUrlCount: 0,
-  });
-
-  console.log("Starting a state machine execution to continue the crawl");
-  await sfn
-    .startExecution({
-      name: `${crawlContext.crawlName}-continued-${new Date()
-        .toISOString()
-        .replace(/[:\.]/g, "-")}`,
-      stateMachineArn: crawlContext.stateMachineArn,
-      input: JSON.stringify({
-        Payload: { crawlContext },
-      }),
-    })
-    .promise();
+export const hotelCrawl = async () => {
+  console.log('banana')
+  const options = {
+  method: 'GET',
+  url: 'https://priceline-com-provider.p.rapidapi.com/v1/hotels/search',
+  params: {sort_order: 'PRICE',location_id:3000035821,date_checkout:'2022-12-12',date_checkin:'2022-12-01',star_rating_ids:'3.0,3.5,4.0,4.5,5.0'},
+  headers: {
+    'X-RapidAPI-Key': '075d9986b1mshf7fe78bb29d44a3p173798jsn90f9820584bd',
+    'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
+  }
 };
+
+axios.request(options).then(function (response) {
+	// console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+};
+
+hotelCrawl()
