2efcb5d3df30bcf9add391e133d91290488656ff
Jonas Braga <jonasbraga2001@gmail.com>
2022-12-02 20:00:44 -0300

    feat: :sparkles: getTrips API

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -1,24 +1,43 @@
+import * as AWS from "aws-sdk";
+
+const {
+  HOTEL_CRAWL_FUNCTION_NAME,
+  TOURS_CRAWL_FUNCTION_NAME,
+  TRAVEL_CRAWL_FUNCTION_NAME,
+  VEHICLE_CRAWL_FUNCTION_NAME,
+} = process.env;
+
 export const getTrips = async (event: any, context: any) => {
   console.log(event, context);
 
-  // TODO....
+  const { city, checkInDate, checkOutDate, rooms } = event.Payload;
 
-  /*
-  var aws = require('aws-sdk');
-  var lambda = new aws.Lambda({
-    region: 'us-west-2' //change to your region
+  const lambda = new AWS.Lambda({
+    region: "us-east-1", //change to your region
   });
 
-  lambda.invoke({
-    FunctionName: 'name_of_your_lambda_function',
-    Payload: JSON.stringify(event, null, 2) // pass params
-  }, function(error, data) {
-    if (error) {
-      context.done('error', error);
-    }
-    if(data.Payload){
-    context.succeed(data.Payload)
-    }
+  const crawlers = [
+    HOTEL_CRAWL_FUNCTION_NAME,
+    //TOURS_CRAWL_FUNCTION_NAME,
+    //TRAVEL_CRAWL_FUNCTION_NAME,
+    //VEHICLE_CRAWL_FUNCTION_NAME,
+  ];
+
+  const promises = crawlers.map((functionName) => {
+    lambda
+      .invoke({
+        FunctionName: functionName!,
+        Payload: JSON.stringify({
+          city,
+          checkInDate,
+          checkOutDate,
+          rooms,
+        }),
+      })
+      .promise();
   });
-  */
+
+  const result = await Promise.allSettled(promises);
+
+  return JSON.stringify(result);
 };
